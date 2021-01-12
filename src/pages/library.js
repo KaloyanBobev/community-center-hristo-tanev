fb453812571ca18a569c2fa7c2cf691cc4840564
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-12 14:30:55 +0000

    Create library.js

---
+++ src/pages/library.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class library extends Component {
+    render() {
+        return (
+            <div>
+                library
+            </div>
+        )
+    }
+}
