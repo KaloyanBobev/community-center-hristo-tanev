c04b7039bb86bdadd42f6bc4d1f3c6bab49782a1
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-12 14:30:58 +0000

    Create membership.js

---
+++ src/pages/membership.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class membership extends Component {
+    render() {
+        return (
+            <div>
+                Membership
+            </div>
+        )
+    }
+}
