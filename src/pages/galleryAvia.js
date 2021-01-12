dbc5ab6627924eb92c5f20d5b1694eeb8772c2ec
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-12 14:30:51 +0000

    Create galleryAvia.js

---
+++ src/pages/galleryAvia.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class galleryAvia extends Component {
+    render() {
+        return (
+            <div>
+                galleryAvia
+            </div>
+        )
+    }
+}
