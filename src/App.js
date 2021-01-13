b303ce1f1a5ef8cf9997d12a593d4590a10ed483
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-13 17:31:06 +0000

    add all the route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { Switch, Route } from 'react-router-dom';
+import './App.scss';
+import Navbar from './components/Navbar';
 
-import './App.css';
+import Home from './pages/home';
+import About from './pages/about';
+import Activity from './pages/activity';
+import GalleryAvia from './pages/galleryAvia';
+import Library from './pages/library';
+import Membership from './pages/membership';
+import notFound from './pages/notFound';
 
 function App() {
   return (
     <div className="app">
-      App
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/activity" component={Activity} />
+        <Route path="/galleryAvia" component={GalleryAvia} />
+        <Route path="/library" component={Library} />
+        <Route path="/membership" component={Membership} />
+        <Route component={notFound} />
+      </Switch>
     </div>
   );
 }
