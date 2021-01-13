3834440793af9134a1044531a8fe4b9d04cd358e
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-13 21:12:21 +0000

    add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,20 @@ export default class Navbar extends Component {
     render() {
         return (
             <nav className="navbar-container">
-                <Link to="/">
-                    <img
-                        src={sign}
-                        className="logo"
-                        alt="logo" />
-                </Link>
-                <Link to="/">Начало</Link>
-                <Link to="/membership">Членство</Link>
-                <Link to="/activity">Дейност</Link>
-                <Link to="/galleryAvia">Алба Авитохол</Link>
-                <Link to="/library">Библиотека</Link>
-                <Link to="/about">За нас</Link>
+                <div className="nav-center">
+                    <Link to="/">
+                        <img
+                            src={sign}
+                            className="logo"
+                            alt="logo" />
+                    </Link>
+                    <Link to="/">Начало</Link>
+                    <Link to="/membership">Членство</Link>
+                    <Link to="/activity">Дейност</Link>
+                    <Link to="/galleryAvia">Алба Авитохол</Link>
+                    <Link to="/library">Библиотека</Link>
+                    <Link to="/about">За нас</Link>
+                </div>
             </nav>
         )
     }
