e6e79a75fc791cc503e23815ac4af8a866088470
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-13 17:30:44 +0000

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+import { Link } from 'react-router-dom';
+import sign from '../images/sign-hristo-tanev.png';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar-container">
+                <Link to="/">
+                    <img
+                        src={sign}
+                        className="logo"
+                        alt="logo" />
+                </Link>
+                <Link to="/">Начало</Link>
+                <Link to="/membership">Членство</Link>
+                <Link to="/activity">Дейност</Link>
+                <Link to="/galleryAvia">Алба Авитохол</Link>
+                <Link to="/library">Библиотека</Link>
+                <Link to="/about">За нас</Link>
+            </nav>
+        )
+    }
+}
