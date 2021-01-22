383980e0f52393a46b463e7bd97f3fcd6f073dca
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-22 22:19:43 +0000

    restore the old navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { ReactNavbar } from "react-responsive-animate-navbar";
 
-//import { Slide } from 'react-slideshow-image';
+import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
-//import sign1 from '../images/sign-hristo-tanev.png';
-//import sign2 from '../images/portret-hristo-tanev.jpg';
-//import Navigation from './Navigation';
+import { FaAlignRight } from 'react-icons/fa';
+import sign1 from '../images/sign-hristo-tanev.png';
+import sign2 from '../images/portret-hristo-tanev.jpg';
+import Navigation from './Navigation';
 import navData from '../data/navData';
 
 
@@ -14,6 +14,7 @@ export default class Navbar extends Component {
         super();
         this.state = {
             nav: navData,
+            isOpen: false
         };
     }
 
@@ -25,84 +26,55 @@ export default class Navbar extends Component {
 
     }
 
+    hadleToggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
     render() {
-
+        console.log(this.isOpen);
         return (
+            <nav className="navbar-container">
 
-            <ReactNavbar
-                color="rgb(25, 25, 25)"
-                logo="https://svgshare.com/i/KHh.svg"
-                menu={[
-                    { name: "HOME", to: "/" },
-                    { name: "ARTICLES", to: "/articles" },
-                    { name: "ABOUT ME", to: "/about" },
-                    { name: "CONTACT", to: "/contact" },
-                ]}
-                menu={[
-                    { name: "HOME1", to: "/" },
-                    { name: "ARTICLES1", to: "/articles" },
-                    { name: "ABOUT ME1", to: "/about" },
-                    { name: "CONTACT1", to: "/contact" },
-                ]}
-                social={[
-                    {
-                        name: "Linkedin",
-                        url: "https://www.linkedin.com/in/nazeh-taha/",
-                        icon: ["fab", "linkedin-in"],
-                    },
-                    {
-                        name: "Facebook",
-                        url: "https://www.facebook.com/nazeh200/",
-                        icon: ["fab", "facebook-f"],
-                    },
-                    {
-                        name: "Instagram",
-                        url: "https://www.instagram.com/nazeh_taha/",
-                        icon: ["fab", "instagram"],
-                    },
-                    {
-                        name: "Twitter",
-                        url: "http://nazehtaha.herokuapp.com/",
-                        icon: ["fab", "twitter"],
-                    },
-                ]}
-            />
-
-            // <nav className="navbar-container">
-            //     <div className="nav-top">
-            //         <Navigation nav={this.state.nav.top} />
-            //     </div>
-            //     <div className="slide-container">
-            //         <img
-            //             src={sign1}
-            //             className="logo1 img-size"
-            //             alt="sign hristo tanev" />
-            //         <Slide className="slide-shadow">
-            //             <div className="each-slide">
-            //                 <div className="slide1 img-size" >
-            //                     <span>Slide 1</span>
-            //                 </div>
-            //             </div>
-            //             <div className="each-slide">
-            //                 <div className="slide2 img-size" >
-            //                     <span>Slide 2</span>
-            //                 </div>
-            //             </div>
-            //             <div className="each-slide">
-            //                 <div className="slide3 img-size" >
-            //                     <span>Slide 3</span>
-            //                 </div>
-            //             </div>
-            //         </Slide>
-            //         <img
-            //             src={sign2}
-            //             className="logo2 img-size"
-            //             alt="hristo tanev" />
-            //     </div>
-            //     <div className="nav-center">
-            //         <Navigation nav={this.state.nav.center} />
-            //     </div>
-            // </nav>
+                <div className="nav-top">
+                    <Navigation nav={this.state.nav.top} />
+                    <button type="button" className="nav-btn">
+                        <FaAlignRight
+                            className="nav-icon"
+                            onClick={this.hadleToggle} />
+                    </button>
+                </div>
+                <div className="slide-container">
+                    <img
+                        src={sign1}
+                        className="logo1 img-size"
+                        alt="sign hristo tanev" />
+                    <Slide className="slide-shadow">
+                        <div className="each-slide">
+                            <div className="slide1 img-size" >
+                                <span>Slide 1</span>
+                            </div>
+                        </div>
+                        <div className="each-slide">
+                            <div className="slide2 img-size" >
+                                <span>Slide 2</span>
+                            </div>
+                        </div>
+                        <div className="each-slide">
+                            <div className="slide3 img-size" >
+                                <span>Slide 3</span>
+                            </div>
+                        </div>
+                    </Slide>
+                    <img
+                        src={sign2}
+                        className="logo2 img-size"
+                        alt="hristo tanev" />
+                </div>
+                <div className="nav-center">
+                    <Navigation nav={this.state.nav.center} />
+                </div>
+            </nav>
         )
     }
 }
