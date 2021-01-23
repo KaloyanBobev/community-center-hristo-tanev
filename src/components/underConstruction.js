a5fd3ba1664d355960029861c93dc0f720a612a8
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-23 21:01:21 +0000

    create under construction component

---
+++ src/components/underConstruction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function UnderCondtruction() {
+
+
+    return (
+        <section className="underconstruction-container">
+            <h1>Under Construction</h1>
+            <h3>Comming Soon</h3>
+        </section>
+    )
+}
