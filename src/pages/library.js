854ae608701d7cb3323dacc4a777e7db4af34058
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-24 21:04:20 +0000

    add under constr component

--- src/pages/library.js
+++ src/pages/library.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import UnderConstruction from '../components/underConstruction';
 export default class library extends Component {
     render() {
         return (
             <div>
-                library
+                <UnderConstruction />
             </div>
         )
     }
