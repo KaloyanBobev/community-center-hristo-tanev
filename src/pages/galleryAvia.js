7de323e5da9cf48840e30179f1b89769cbd7ddc5
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-26 20:47:56 +0000

    create the code

--- src/pages/galleryAvia.js
+++ src/pages/galleryAvia.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
-import UnderConstruction from '../components/underConstruction';
+import albaData from '../data/albaData.js';
+import Container from '../components/Container';
+import ToggleBtn from '../components/ToggleBtn';
+
 export default class galleryAvia extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            visible: true
+        }
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+    componentDidMount() {
+        this.setState({
+            data: albaData
+        })
+    }
+    handleToggle() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
     render() {
+
         return (
-            <div>
-                <UnderConstruction />
-            </div>
+            <>
+                <section className="alba-container">
+                    <h1>Васа РУСЕВА, галерия „Алба-Авитохол“ в Стара Загора: Редовните ни срещи продължават</h1>
+                    <Container data={this.state.data} />
+
+                </section>
+                <ToggleBtn
+                    handleToggle={this.handleToggle}
+                    visible={this.state.visible}
+                />
+            </>
         )
     }
 }
