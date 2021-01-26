2edb4b73814a9b739c147ed47cb720f4ee723312
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-26 16:34:49 +0000

    create Container component

---
+++ src/components/Container.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Container({ data }) {
+    return (
+        <>
+            {data.map((item) => {
+                return (
+                    <p key={item.sys.id}>
+                        {item.fields.text}
+                    </p>
+                )
+            })}
+        </>
+    )
+}
