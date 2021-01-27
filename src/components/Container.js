7619b97b9b3c3bb2a2ef2d368c54f19256124753
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-27 16:20:16 +0000

    rearenging the code

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-export default function Container({ data }) {
+export default function Container({ visible, data, heading }) {
     return (
-        <>
+        <section className={visible
+            ? "container"
+            : "container show"}>
+            <h1>{heading}</h1>
             {data.map((item) => {
                 return (
                     <p key={item.sys.id}>
@@ -10,6 +13,6 @@ export default function Container({ data }) {
                     </p>
                 )
             })}
-        </>
+        </section>
     )
 }
