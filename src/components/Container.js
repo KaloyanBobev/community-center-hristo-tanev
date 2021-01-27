b4ab59fa5235412daae64196c3e4bcce94f8791d
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-27 17:32:30 +0000

    Update Container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-export default function Container({ visible, data, heading }) {
+export default function Container({ className, visible, data, heading }) {
     return (
         <section className={visible
             ? "container"
             : "container show"}>
             <h1>{heading}</h1>
-            {data.map((item) => {
-                return (
-                    <p key={item.sys.id}>
-                        {item.fields.text}
-                    </p>
-                )
-            })}
+            <div className={className}>
+                {data.map((item) => {
+                    return (
+                        <p key={item.sys.id}>
+                            {item.fields.text}
+                        </p>
+                    )
+                })}
+            </div>
         </section>
     )
 }
