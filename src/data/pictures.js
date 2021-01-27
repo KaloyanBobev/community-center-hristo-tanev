c1abab790b30e1522e88c96c3d2758425e4d18e4
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-27 16:19:33 +0000

    Create pictures.js

---
+++ src/data/pictures.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class pictures extends Component {
+    render() {
+        return (
+            <section className="pictures-container">
+                <h1>Картини на Христо Танев</h1>
+                <div className="image-center">
+
+                </div>
+            </section>
+        )
+    }
+}
