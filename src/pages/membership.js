bdf4762806d4932cba7b6c74650d6f2a90ea8d73
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-30 18:38:42 +0000

    add paypall buttons

--- src/pages/membership.js
+++ src/pages/membership.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 
-import UnderConstruction from '../components/underConstruction';
+import { PayPalButton } from "react-paypal-button-v2";
 
 export default class membership extends Component {
     render() {
         return (
-            <div>
-                <UnderConstruction />
-            </div>
+            <section className="paypal-container">
+                <h1>Заплащане на месечен членски внос</h1>
+                <p>За заплащането на членския внос могат да с изпозват следните методи</p>
+                <PayPalButton
+                    amount="0.01"
+                    // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+                    onSuccess={(details, data) => {
+                        alert("Transaction completed by " + details.payer.name.given_name);
+
+                        // OPTIONAL: Call your server to save the transaction
+                        return fetch("/paypal-transaction-complete", {
+                            method: "post",
+                            body: JSON.stringify({
+                                orderID: data.orderID
+                            })
+                        });
+                    }}
+                />
+            </section>
         )
     }
 }
