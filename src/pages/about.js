6b31e246c3d46bbcdb5256c023b17e275570c9a5
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-30 18:39:26 +0000

    add information

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,12 @@ import UnderConstruction from '../components/underConstruction';
 export default class about extends Component {
     render() {
         return (
-            <div>
-                <UnderConstruction />
-            </div>
+            <section className="about-container">
+                <h1>Контакти</h1>
+                <h3>адрес</h3>
+                <h3>телефон за контакт</h3>
+                <h3>GSM за контакт</h3>
+            </section>
         )
     }
 }
