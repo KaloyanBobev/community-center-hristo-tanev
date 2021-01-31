b60d162f74f5f97cb8648f347fd931fba3b4f1e0
kaloyanbobev <kaloyanbobev@gmail.com>
2021-01-31 13:47:45 +0000

    add google map

---
+++ src/components/googleMap.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function googleMap() {
+    return (
+        <div>
+            <iframe title="community center"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d736.3596374086586!2d25.61543122922663!3d42.418399288754536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDLCsDI1JzA2LjIiTiAyNcKwMzYnNTcuNSJF!5e0!3m2!1sbg!2suk!4v1590675402039!5m2!1sbg!2suk"
+                frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+        </div>
+    )
+}
