291fb70f3a4f88919e889a0a3a3e0e3bc951a126
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-03 19:00:54 +0000

    create image component

---
+++ src/components/Image.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+export default class Image extends Component {
+    render() {
+        return (
+            <>
+                <h1>Фото галерия</h1>
+                <a>
+                    <img src="" alt=""></img>
+                </a>
+            </>
+        )
+    }
+}
