c51ba72bde340de4969ad272bc6dfc2b2593e2d8
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-04 13:30:25 +0000

    add new images

---
+++ src/data/imageData.js
@@ -0,0 +1,97 @@
+
+import img1 from '../images/img/img1.jpg';
+import img2 from '../images/img/img2.jpg';
+import img3 from '../images/img/img3.jpg';
+import img4 from '../images/img/img4.jpg';
+import img5 from '../images/img/img5.jpg';
+import img6 from '../images/img/img6.jpg';
+import img7 from '../images/img/img7.jpg';
+import img8 from '../images/img/img8.jpg';
+import img9 from '../images/img/img9.jpg';
+import img10 from '../images/img/img10.jpg';
+import img11 from '../images/img/img11.jpg';
+import img12 from '../images/img/img12.jpg';
+import img13 from '../images/img/img13.jpg';
+import img14 from '../images/img/img14.jpg';
+import img15 from '../images/img/img15.jpg';
+import img16 from '../images/img/img16.jpg';
+import img17 from '../images/img/img17.jpg';
+import img18 from '../images/img/img18.jpg';
+import img19 from '../images/img/img19.jpg';
+import img20 from '../images/img/img20.jpg';
+import img21 from '../images/img/img21.jpg';
+
+
+
+// eslint-disable-next-line
+export default [
+    {
+        sys: {
+            id: "1",
+
+        },
+        fields: {
+            name: "",
+            src: img1
+        }
+    }, {
+        sys: {
+            id: "2",
+
+        },
+        fields: {
+            name: "",
+            src: img2
+        }
+    },
+    {
+        sys: {
+            id: "3",
+
+        },
+        fields: {
+            name: "",
+            src: img3
+        }
+    },
+    {
+        sys: {
+            id: "4",
+
+        },
+        fields: {
+            name: "",
+            src: img4
+        }
+    },
+    {
+        sys: {
+            id: "5",
+
+        },
+        fields: {
+            name: "",
+            src: img5
+        }
+    },
+    {
+        sys: {
+            id: "6",
+
+        },
+        fields: {
+            name: "",
+            src: img6
+        }
+    },
+    {
+        sys: {
+            id: "7",
+
+        },
+        fields: {
+            name: "",
+            src: img7
+        }
+    }
+]
