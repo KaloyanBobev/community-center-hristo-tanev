2844c28057cd4c1f57730afd1b01fc12a1690c83
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-04 18:09:25 +0000

    create the backbone

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 
-
+import imgData from '../data/imageData'
 
 export default class Image extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            data: imgData
+        })
+    }
     render() {
         return (
             <>
                 <h1>Фото галерия</h1>
-                <a>
-                    <img src="" alt=""></img>
-                </a>
+                {imgData.map((img) => {
+                    return (
+                        <a key={img.sys.id} href={img.fields.src}>
+                            <img src={img.fields.src} alt={img.fields.name}></img>
+                        </a>
+                    )
+                })}
             </>
         )
     }
