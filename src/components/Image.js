61c7e38852cc78a9541059077e83fa2abef4d8b8
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-08 18:11:47 +0000

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -23,10 +23,10 @@ export default class Image extends Component {
                     return (
                         <a
                             target="_blank"
+                            rel="noreferrer"
                             key={img.sys.id}
                             href={img.fields.src}>
                             <img className="img"
-
                                 src={img.fields.src}
                                 alt={img.fields.name}></img>
                         </a>
