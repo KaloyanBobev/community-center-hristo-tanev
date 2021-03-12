529fe1fa6eb34a6cec750d8a5fa0c55e39bd6c46
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-12 19:12:07 +0000

    create data for docs images

---
+++ src/data/membershipData.js
@@ -0,0 +1,35 @@
+import img1 from '../images/membership/zavlenie-image.png'
+import img2 from '../images/membership/ustav-image.png'
+import img3 from '../images/membership/udostoverenie-image.png'
+
+export default [
+    {
+        sys: {
+            id: "1",
+
+        },
+        fields: {
+            name: "Заявление",
+            src: img1
+        }
+    }, {
+        sys: {
+            id: "2",
+
+        },
+        fields: {
+            name: "Устав",
+            src: img2
+        }
+    },
+    {
+        sys: {
+            id: "3",
+
+        },
+        fields: {
+            name: "Удостоверение",
+            src: img3
+        }
+    }
+]
