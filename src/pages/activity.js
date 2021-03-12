50e97830793b048219623262af56ca50ed0d105d
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-12 19:11:31 +0000

    add data prop

--- src/pages/activity.js
+++ src/pages/activity.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Image from '../components/Image';
+import imgData from '../data/imageData';
 export default class activity extends Component {
     render() {
         return (
             <section className="activity-container">
                 <h1>Фото галерия</h1>
-                <Image />
+                <Image data={imgData} />
             </section>
         )
     }
