c739c294a7c5522ca4a5312f6da706bd612b8fe3
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-14 21:33:48 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,11 @@ import SocialFollow from './SocialFollow';
 
 export default function footer() {
     let year = new Date().getFullYear();
+    let baseYear = "2020";
     return (
         <footer className="footer-container">
             <SocialFollow />
-            <p>Всички права запазени. CopyRight © 2020 - {year} година</p>
+            <p>Всички права запазени. CopyRight © {baseYear} - {year} година</p>
         </footer>
     )
 }
