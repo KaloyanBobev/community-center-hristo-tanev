e9ac3110b45698c126cf20bf90bd0c89fa83e2b0
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-15 16:24:51 +0000

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,10 +5,10 @@ export default class about extends Component {
     render() {
         return (
             <section className="about-container">
-                <h1>Контакти</h1>
-                <h3>адрес</h3>
-                <h3>телефон за контакт</h3>
-                <h3>GSM за контакт</h3>
+                <h1>Контакти: </h1>
+                <h3>Aдрес: </h3>
+                <h3>Tелефон за контакт: </h3>
+                <h3>GSM за контакт: </h3>
                 <GoogleMap />
             </section>
         )
