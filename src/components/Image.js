af072660432d89db7e908c7a4a6112b91fbb9688
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-15 16:25:20 +0000

    remove constructor

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 
 
 export default class Image extends Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-
     render() {
         return (
             <>
