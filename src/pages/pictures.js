13ee7ceb2adc624201c6b2313cc53f91cdf58c07
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-17 17:00:20 +0000

    add component Image and data for more pivtures

--- src/pages/pictures.js
+++ src/pages/pictures.js
@@ -1,20 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Image from '../components/Image';
 import img1 from '../images/pictures/Hristo-Tanev-Zakrilnikyt na Obetowanata zemq.jpg';
-
+import pictureData from '../data/picturesData';
 export default class pictures extends Component {
     render() {
         return (
             <section className="pictures-container">
                 <h1>Картини на Христо Танев</h1>
-                <div className="image-center ">
-                    <a href={img1}
-                        rel="noreferrer"
-                        target="_blank">
-                        <img className="img"
-                            src={img1}
-                            alt="art" />
-                    </a>
-                </div>
+                <Image data={pictureData} />
             </section>
         )
     }
