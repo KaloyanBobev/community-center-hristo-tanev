50f4111544202611f1b01d121b9e4281504be1ec
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-17 17:14:42 +0000

    Update pictures.js

--- src/pages/pictures.js
+++ src/pages/pictures.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Image from '../components/Image';
-import img1 from '../images/pictures/Hristo-Tanev-Zakrilnikyt na Obetowanata zemq.jpg';
+
 import pictureData from '../data/picturesData';
 export default class pictures extends Component {
     render() {
