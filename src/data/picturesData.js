4371e7a0dfe93cc3091bcc875708726eb64f017e
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-17 17:02:15 +0000

    create ne data file for pictures

---
+++ src/data/picturesData.js
@@ -0,0 +1,160 @@
+
+import img1 from '../images/pictures/Daniela-Dokcheva-Hestinarsko-horo.jpg';
+import img2 from '../images/pictures/Daniela-Dokcheva-Peperudeno.jpg';
+import img3 from '../images/pictures/Daniela-Dokcheva-Tanc.jpg';
+import img4 from '../images/pictures/goblen 1.jpg';
+import img5 from '../images/pictures/goblen 2.jpg';
+import img6 from '../images/pictures/goblen 3.jpg';
+import img7 from '../images/pictures/goblen 4.jpg';
+import img8 from '../images/pictures/goblen5.jpg';
+import img9 from '../images/pictures/Hristo-Tanev-Probudenica.jpg';
+import img10 from '../images/pictures/Hristo-Tanev-Zakrilnikyt na Obetowanata zemq.jpg';
+import img11 from '../images/pictures/Milena-Gospodinova-risunka.jpg';
+import img12 from '../images/pictures/Sasho-Sandev-Cvete.jpg';
+import img13 from '../images/pictures/Svetlin-Stoev.jpg';
+import img14 from '../images/pictures/Vasa-Ruseva.jpg';
+
+
+
+// eslint-disable-next-line
+export default [
+    {
+        sys: {
+            id: "1",
+
+        },
+        fields: {
+            name: "",
+            src: img1
+        }
+    }, {
+        sys: {
+            id: "2",
+
+        },
+        fields: {
+            name: "",
+            src: img2
+        }
+    },
+    {
+        sys: {
+            id: "3",
+
+        },
+        fields: {
+            name: "",
+            src: img3
+        }
+    },
+    {
+        sys: {
+            id: "4",
+
+        },
+        fields: {
+            name: "",
+            src: img4
+        }
+    },
+    {
+        sys: {
+            id: "5",
+
+        },
+        fields: {
+            name: "",
+            src: img5
+        }
+    },
+    {
+        sys: {
+            id: "6",
+
+        },
+        fields: {
+            name: "",
+            src: img6
+        }
+    },
+    {
+        sys: {
+            id: "7",
+
+        },
+        fields: {
+            name: "",
+            src: img7
+        }
+    },
+    {
+        sys: {
+            id: "8",
+
+        },
+        fields: {
+            name: "",
+            src: img8
+        }
+    },
+    {
+        sys: {
+            id: "9",
+
+        },
+        fields: {
+            name: "",
+            src: img9
+        }
+    },
+    {
+        sys: {
+            id: "10",
+
+        },
+        fields: {
+            name: "",
+            src: img10
+        }
+    },
+    {
+        sys: {
+            id: "11",
+
+        },
+        fields: {
+            name: "",
+            src: img11
+        }
+    },
+    {
+        sys: {
+            id: "12",
+
+        },
+        fields: {
+            name: "",
+            src: img12
+        }
+    },
+    {
+        sys: {
+            id: "13",
+
+        },
+        fields: {
+            name: "",
+            src: img13
+        }
+    },
+    {
+        sys: {
+            id: "14",
+
+        },
+        fields: {
+            name: "",
+            src: img14
+        }
+    }
+]
