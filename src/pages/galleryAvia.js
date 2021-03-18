85e502e4e1a3d10a32fcdbca6c164846d7113357
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-18 11:37:00 +0000

    remove the text and add the pictures

--- src/pages/galleryAvia.js
+++ src/pages/galleryAvia.js
@@ -1,44 +1,17 @@
-import React, { Component } from 'react'
-import albaData from '../data/albaData.js';
-import Container from '../components/Container';
-import ToggleBtn from '../components/ToggleBtn';
+import React, { Component } from 'react';
 
+import Image from '../components/Image';
+import data from '../data/picturesData';
 export default class galleryAvia extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            data: [],
-            visible: true
-        }
-        this.handleToggle = this.handleToggle.bind(this);
-    }
-    componentDidMount() {
-        this.setState({
-            data: albaData
-        })
-    }
-    handleToggle() {
-        this.setState({
-            visible: !this.state.visible
-        })
-    }
-    render() {
-        let heading = "Васа РУСЕВА, галерия „Алба-Авитохол“ в Стара Загора: Редовните ни срещи продължават";
-        return (
-            <>
-                <Container
-                    className={"alba-text"}
-                    visible={this.state.visible}
-                    data={this.state.data}
-                    heading={heading}
-                />
 
-                <ToggleBtn
-                    handleToggle={this.handleToggle}
-                    visible={this.state.visible}
-                />
-            </>
+    render() {
+
+        return (
+            <section className="pictures-container">
+                <h1>Картини представени в Алба Авитохол</h1>
+                <Image data={data} />
+            </section>
         )
     }
 }
