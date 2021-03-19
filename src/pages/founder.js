a645d4f349abe16add0870676327e87f7371531b
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-19 20:38:06 +0000

    create new page founder

---
+++ src/pages/founder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class founder extends Component {
+    render() {
+        return (
+            <section className="container">
+                <h1>Founder</h1>
+            </section>
+        )
+    }
+}
+
