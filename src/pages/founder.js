3ed730968b6f38a664d8fb5174bd3cee57856206
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-21 21:34:42 +0000

    add image component

--- src/pages/founder.js
+++ src/pages/founder.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import Image from '../components/Image';
+import data from '../data/founderData';
 export default class founder extends Component {
     render() {
         return (
             <section className="container">
                 <h1>Founder</h1>
+                <Image data={data} />
             </section>
         )
     }
