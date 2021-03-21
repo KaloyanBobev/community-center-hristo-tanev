ee5e2f61e328275bf4b602364f7fd8bfca25e879
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-21 21:35:17 +0000

    create data for hristo's portraits

---
+++ src/data/founderData.js
@@ -0,0 +1,28 @@
+import img1 from '../images/hristo-portrets/Hristo-Tanev-Probudenica.jpg';
+import img2 from '../images/hristo-portrets/portret-hristo-tanev.jpg';
+
+
+
+
+// eslint-disable-next-line
+export default [
+    {
+        sys: {
+            id: "1",
+
+        },
+        fields: {
+            name: "",
+            src: img1
+        }
+    }, {
+        sys: {
+            id: "2",
+
+        },
+        fields: {
+            name: "",
+            src: img2
+        }
+    }
+]
