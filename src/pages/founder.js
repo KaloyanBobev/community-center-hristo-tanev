8a4dc58db2b412367a3d9b106fbd4308cf912de4
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-23 11:55:42 +0000

    add constructor, functions and components

--- src/pages/founder.js
+++ src/pages/founder.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react'
 import Image from '../components/Image';
 import Container from '../components/Container';
+import ToggleBtn from '../components/ToggleBtn';
 import data from '../data/founderData';
 export default class founder extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            visible: true
+        }
+        this.handleToggle = this.handleToggle.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            data: data
+        })
+
+    }
+
+    handleToggle() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
     render() {
+        let heading = "За Христо Танев";
         return (
-            <section className="container">
-                <h1>Founder</h1>
-                <Image data={data} />
-                <h1>За Христо Танев</h1>
-                <Container />
+            <section className="container founder">
+                <div>
+                    <h1>Founder</h1>
+                    <Image data={data.slice(0, 2)} />
+                </div>
+
+                <div>
+                    <Container
+                        n
+                        visible={this.state.visible}
+                        data={this.state.data.slice(2, 12)}
+                        heading={heading}
+                    />
+                    <ToggleBtn
+                        visible={this.state.visible}
+                        handleToggle={this.handleToggle}
+                    />
+                </div>
             </section>
         )
     }
