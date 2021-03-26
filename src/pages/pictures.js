016add90dcefe1500675179fc5897f9007e5cd6e
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-26 17:07:29 +0000

    add component Image and database

--- src/pages/pictures.js
+++ src/pages/pictures.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
-// import Image from '../components/Image';
+import Image from '../components/Image';
 
-// import pictureData from '../data/picturesData';
-import img from '../images/hristo-pictures/Hristo-Tanev-Zakrilnikyt na Obetowanata zemq.jpg';
+import data from '../data/picturesHristoTanevData';
 export default class pictures extends Component {
     render() {
         return (
             <section className="pictures-container">
                 <h1>Картини на Христо Танев</h1>
-                <img className="img" alt="HristoTanev" src={img} />
+                {/* <img className="img" alt="HristoTanev" src={img} /> */}
+
+                <Image data={data}
+                    isShow={true}
+                />
             </section>
         )
     }
