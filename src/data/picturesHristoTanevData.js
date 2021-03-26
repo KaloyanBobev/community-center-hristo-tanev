147f9076fd02adc5726e1013a9cb4e0361314b91
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-26 17:07:49 +0000

    create new data for Hristo's pictures

---
+++ src/data/picturesHristoTanevData.js
@@ -0,0 +1,14 @@
+import img1 from '../images/hristo-pictures/Hristo-Tanev-Zakrilnikyt na Obetowanata zemq.jpg';
+// eslint-disable-next-line
+export default [
+    {
+        sys: {
+            id: "1",
+            isShow: true
+        },
+        fields: {
+            name: "The protector of the promised land",
+            src: img1
+        }
+    }
+]
