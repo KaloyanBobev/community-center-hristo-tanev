30228ffbb6f78579d1334ec04a67b68d0ba1c48f
kaloyanbobev <kaloyanbobev@gmail.com>
2021-03-30 20:46:52 +0100

    add subheading

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export default function Container({ className, visible, data, heading }) {
+export default function Container({ className, visible, data, heading, subheading }) {
     return (
         <section className={visible
             ? "container"
             : "container show"}>
             <h1>{heading}</h1>
+            <h1>{subheading}</h1>
             <div className={className}>
                 {data.map((item) => {
                     return (
