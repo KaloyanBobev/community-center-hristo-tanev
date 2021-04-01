e2ea3c6c0b8a957e61d551b145bf300aa5448712
kaloyanbobev <kaloyanbobev@gmail.com>
2021-04-01 21:14:47 +0100

    Update galleryAvia.js

--- src/pages/galleryAvia.js
+++ src/pages/galleryAvia.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 
 import Image from '../components/Image';
 import data from '../data/picturesData';
+
 export default class galleryAvia extends Component {
-
-
     render() {
-
-        return (
+       return (
             <section className="picture-body">
                 <h1>Картини представени в Алба Авитохол</h1>
                 <div className="pictures-container">
                     <Image
-
                         data={data}
                         isShow={true} />
                 </div>
