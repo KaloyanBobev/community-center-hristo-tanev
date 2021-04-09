cf0b82afd96551e9d7ea73791569af82a8c85d15
kaloyanbobev <kaloyanbobev@gmail.com>
2021-04-09 20:53:03 +0100

    change the coordiantes

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -4,7 +4,7 @@ export default function googleMap() {
     return (
         <div>
             <iframe title="community center"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d736.3596374086586!2d25.6247669!3d42.4295555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDLCsDI1JzA2LjIiTiAyNcKwMzYnNTcuNSJF!5e0!3m2!1sbg!2suk!4v1590675402039!5m2!1sbg!2suk"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d736.3596374086586!2d25.6242598!3d42.4295892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDLCsDI1JzA2LjIiTiAyNcKwMzYnNTcuNSJF!5e0!3m2!1sbg!2suk!4v1590675402039!5m2!1sbg!2suk"
                 frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
         </div>
     )
