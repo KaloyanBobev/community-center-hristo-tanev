7776c78601b6a710a115443e8ee3ec8c371cd7de
kaloyanbobev <kaloyanbobev@gmail.com>
2021-04-09 20:52:49 +0100

    add email

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,7 @@ export default class about extends Component {
                 <h3>Aдрес: Стара Загора, бул. Методи Кусев 49 </h3>
                 <h3>Tелефон за контакт: </h3>
                 <h3>GSM за контакт: </h3>
+                <h3>Email:chitalishtehristotanev@abv.bg </h3>
                 <GoogleMap />
             </section>
         )
